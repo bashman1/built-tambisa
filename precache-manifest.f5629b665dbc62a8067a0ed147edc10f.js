b26ea4debb8b05b71d33d52c321ae5188b014233
Wamula Bashir Saidi <wamulabash1@gmail.com>
2023-07-15 00:19:19 +0300

    deploying

---
+++ precache-manifest.f5629b665dbc62a8067a0ed147edc10f.js
@@ -0,0 +1,118 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8ae60859eedf9b84ef63dee28a6391b",
+    "url": "./index.html"
+  },
+  {
+    "revision": "e0d34871b17e28b77a1a",
+    "url": "./static/js/2.e46a6480.chunk.js"
+  },
+  {
+    "revision": "aa4100970f46d7e0ec2af84de3f2740b",
+    "url": "./static/js/2.e46a6480.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dfc87b82a79bf97336cc",
+    "url": "./static/js/main.1e805762.chunk.js"
+  },
+  {
+    "revision": "b82965fe5bbfa16eef47",
+    "url": "./static/js/runtime-main.456d1954.js"
+  },
+  {
+    "revision": "fd4f5f325814bf3cf5588cb8b0efd8cd",
+    "url": "./static/media/2018_11_08-Papua-New-Guinea-r3-2357.fd4f5f32.jpg"
+  },
+  {
+    "revision": "982d631407799968c61e087936e9aa40",
+    "url": "./static/media/Tambiisa-Logo-transformed.982d6314.png"
+  },
+  {
+    "revision": "cf9dab794fde0ec6590409f0666dcd96",
+    "url": "./static/media/about.cf9dab79.jpg"
+  },
+  {
+    "revision": "f85903d473cb19505e2aa6c63a0db00b",
+    "url": "./static/media/athlete-small.f85903d4.png"
+  },
+  {
+    "revision": "6d53898d42640dad92347101b160da7f",
+    "url": "./static/media/athlete2.6d53898d.png"
+  },
+  {
+    "revision": "d52fa22226a4326c6570eaf6822dc734",
+    "url": "./static/media/bg_1.d52fa222.jpg"
+  },
+  {
+    "revision": "637b8f8baf6e95478ceb6322eeb2e370",
+    "url": "./static/media/bg_3.637b8f8b.jpg"
+  },
+  {
+    "revision": "44c3e878be1e8f1021b111dddd4b8dd8",
+    "url": "./static/media/cover2.44c3e878.png"
+  },
+  {
+    "revision": "c88beaa2183fde7f59e6203751e872dc",
+    "url": "./static/media/dry-vanilla.c88beaa2.jpg"
+  },
+  {
+    "revision": "689e0e7cb1fa683cf40000056ef615e7",
+    "url": "./static/media/getPaidStock.689e0e7c.png"
+  },
+  {
+    "revision": "499f60987c621c2335d77935d5a3eb1b",
+    "url": "./static/media/ginger-plant-compressed.499f6098.webp"
+  },
+  {
+    "revision": "cb52af0ac5bc779b84eb6b73fb921da2",
+    "url": "./static/media/ginger_600x.cb52af0a.webp"
+  },
+  {
+    "revision": "e149f59c6323b18f98e987cd564e1fc9",
+    "url": "./static/media/good-times2.e149f59c.jpg"
+  },
+  {
+    "revision": "169a9b3ae8eff8e916dc7711c7546c64",
+    "url": "./static/media/goodtimes-small.169a9b3a.png"
+  },
+  {
+    "revision": "bbcd5413ffcc46f7d9eef5fb36340659",
+    "url": "./static/media/person_1.bbcd5413.jpg"
+  },
+  {
+    "revision": "2cd3dcd36bb7c61c847ac770c2aff481",
+    "url": "./static/media/person_2.2cd3dcd3.jpg"
+  },
+  {
+    "revision": "47e2c88c847a0b1a817ca6636cfe63dc",
+    "url": "./static/media/product-1.47e2c88c.jpg"
+  },
+  {
+    "revision": "28f609b2c78ee06f368594b82932d3bc",
+    "url": "./static/media/product-2.28f609b2.jpg"
+  },
+  {
+    "revision": "5cde8300197141a7614325db2468d601",
+    "url": "./static/media/product-3.5cde8300.jpg"
+  },
+  {
+    "revision": "81c0171a934ad323c4fb3cc1090daf69",
+    "url": "./static/media/product-4.81c0171a.jpg"
+  },
+  {
+    "revision": "3d742708f0cd805650c2e4a2bd0e0bf7",
+    "url": "./static/media/product-5.3d742708.jpg"
+  },
+  {
+    "revision": "74873026dbed7229712447d165a7a2fd",
+    "url": "./static/media/the-racer2.74873026.jpg"
+  },
+  {
+    "revision": "ca518f44c6e1ddc450654002971bb467",
+    "url": "./static/media/theracer-small.ca518f44.png"
+  },
+  {
+    "revision": "0b9516e2b9d8b550e2ad4252879c735e",
+    "url": "./static/media/truck.0b9516e2.gif"
+  }
+]);
