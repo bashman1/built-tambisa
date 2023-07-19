295e9ea7128e620a1a64ca6114b23231765d8a10
Wamula Bashir Saidi <wamulabash1@gmail.com>
2023-07-19 11:10:46 +0300

    deploying

---
+++ precache-manifest.fdaf1f2b8ae383cf1bda957eeb40c1a8.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7b16ecba9c48fe7d9efcdf54da33612",
+    "url": "./index.html"
+  },
+  {
+    "revision": "c2e5f174429a2814262e",
+    "url": "./static/js/2.44058367.chunk.js"
+  },
+  {
+    "revision": "158bd9b592b19fd93cfe670ffa11fc42",
+    "url": "./static/js/2.44058367.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ce691570602a76c55931",
+    "url": "./static/js/main.26ec5b66.chunk.js"
+  },
+  {
+    "revision": "b82965fe5bbfa16eef47",
+    "url": "./static/js/runtime-main.456d1954.js"
+  },
+  {
+    "revision": "cf9dab794fde0ec6590409f0666dcd96",
+    "url": "./static/media/about.cf9dab79.jpg"
+  },
+  {
+    "revision": "f85903d473cb19505e2aa6c63a0db00b",
+    "url": "./static/media/athlete-small.f85903d4.png"
+  },
+  {
+    "revision": "6d53898d42640dad92347101b160da7f",
+    "url": "./static/media/athlete2.6d53898d.png"
+  },
+  {
+    "revision": "d52fa22226a4326c6570eaf6822dc734",
+    "url": "./static/media/bg_1.d52fa222.jpg"
+  },
+  {
+    "revision": "637b8f8baf6e95478ceb6322eeb2e370",
+    "url": "./static/media/bg_3.637b8f8b.jpg"
+  },
+  {
+    "revision": "cb52af0ac5bc779b84eb6b73fb921da2",
+    "url": "./static/media/ginger_600x.cb52af0a.webp"
+  },
+  {
+    "revision": "e149f59c6323b18f98e987cd564e1fc9",
+    "url": "./static/media/good-times2.e149f59c.jpg"
+  },
+  {
+    "revision": "169a9b3ae8eff8e916dc7711c7546c64",
+    "url": "./static/media/goodtimes-small.169a9b3a.png"
+  },
+  {
+    "revision": "74873026dbed7229712447d165a7a2fd",
+    "url": "./static/media/the-racer2.74873026.jpg"
+  },
+  {
+    "revision": "ca518f44c6e1ddc450654002971bb467",
+    "url": "./static/media/theracer-small.ca518f44.png"
+  }
+]);
